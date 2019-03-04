dc2a37a76d5618eb1568bda47dbe203d164e1cce
Peter Jang <peter.x.jang@gmail.com>
2019-03-04 15:07:52 -0600

    Add sample home pagewith image

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <img src="red-banana-benefits_620x350_41498045864.jpg" alt="">
+    <div v-for="product in products">
+      <p>Id: {{ product.id }}</p>
+      <p>Name: {{product.name }}</p>
+    </div>
   </div>
 </template>
 
+<style>
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      products: []
+    };
+  },
+  created: function() {
+    axios.get("/api/products").then(response => {
+      this.products = response.data;
+    });
+  },
+  methods: {}
+};
 </script>
